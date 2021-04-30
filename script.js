1e673c04ce34a610cfee2a02871122180455c243
GabrielNarcizo <eligcan@hotmail.com>
2021-04-30 02:02:17 -0300

    criando a cobrinha

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let cobra = [];
+cobra[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG() {
     context.fillStyle = 'lightgreen';
-    context.fillReact(0, 0, 16 * box, 16 * box);
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function criarCobra() {
+    for (i=0; i < cobra.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(cobra[i].x, cobra[i].y, box, box)
+    }
 }
 
 criarBG();
+criarCobra();
